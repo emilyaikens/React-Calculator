b1b0e8f45746c482c2d2801687783fa459bbc7f7
emilyaikens <emily.c.aikens@gmail.com>
2022-09-28 12:21:51 -0700

    import all components to app.js and basic structure is there

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,11 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import Body from "./Body";
+import Display from "./Display";
+import ButtonContainer from "./ButtonContainer";
+import Button from "./Button";
 
 function App() {
 
   return (
     <div className="App">
-      <h1>Calculator App</h1>
+      <Body>
+        <Display value="0" />
+        <ButtonContainer>
+          <Button className="" value="0" 
+              onClick={() => {console.log("button clicked")}}/>
+        </ButtonContainer>
+      </Body>
     </div>
   );
 }
