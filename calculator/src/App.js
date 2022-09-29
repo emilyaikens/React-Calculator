10256490a0d10bbee04bd682b7917731f315a87b
emilyaikens <emily.c.aikens@gmail.com>
2022-09-28 20:23:14 -0700

    add numClickHandler, dotClickHandler, signClickHandler

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -15,6 +15,50 @@ const buttonValues = [
 
 function App() {
 
+  // this handler gets triggered if any of the number are clicked. It gets
+  // the value of the button and adds that to the current number value
+  const numClickHandler = (n) => {
+    n.preventDefault();
+    const value = n.target.innerHTML; //gets value from button
+    if (calculation.num.length < 16) {
+      setCalculation({
+        ...calculation, 
+        num: calculation.num === 0 && value === '0' 
+        ? '0' 
+        : calculation.num % 1 === 0
+        ? Number(calculation.num + value)
+        : calculation.num + value,
+        result: !calculation.sign ? 0 : calculation.result,
+      });
+    };
+  };
+
+  // this handler gets triggered when a decimal point is clicked. 
+  const dotClickHandler = (d) => {
+    d.preventDefault();
+    const value = d.target.innerHTML;
+    setCalculation({
+      ...calculation,
+      num: !calculation.num.toString().includes('.') //only add to value if there's not already a .
+      ? calculation.num + value : calculation.num,
+    });
+  };
+
+  // this handler gets triggered when a sign is pressed (plus, minus, divide, multiply)
+  const signClickHandler = (s) => {
+    s.preventDefault();
+    const value = s.target.innerHTML;
+    setCalculation ({
+      ...calculation,
+      sign: value, 
+      result: ! calculation.result && calculation.num 
+      ? calculation.num : calculation.result,
+      num: 0,
+    });
+  };
+
+
+  // ********** use state *********************************** //
   const [calculation, setCalculation] = useState ({
     sign: '',
     num: 0, 
@@ -45,7 +89,7 @@ function App() {
                         : button === "÷" || button === "×" || button === "-" || button === "+"
                         ? signClickHandler
                         : button === "."
-                        ? commaClickHandler
+                        ? dotClickHandler
                         : numClickHandler
                     }
                   />
